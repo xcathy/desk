ad5c515ac8482cf48e7d5642e045c21a8ddb2f7d
xcathy <cathyxie1997@hotmail.com>
2026-03-13 16:25:36 +0800

    updated scripting for menu

--- Assets/UI/UI.cs
+++ Assets/UI/UI.cs
@@ -1,33 +1,46 @@
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class UI : MonoBehaviour
 {
     // Input vars
     private Keyboard keyboard;
+    private Mouse mouse;
     // Reference vars
     private VisualElement root;
     private VisualElement menu;
-    private Button startover;
-    private Button resume;
-    private Button exit;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Button startoverBtn;
+    private Button resumeBtn;
+    private Button exitBtn;
+
+    // Bools
+    private bool startover;
+    private bool resume;
+    private bool exit;
+
     void Start()
     {
         // referencing the inputs
         keyboard =  Keyboard.current;
+        mouse = Mouse.current;
 
         // referencing the UI elements
         root = GetComponent<UIDocument>().rootVisualElement;
         menu = root.Q<VisualElement>("menu");
 
-        startover = menu.Q<Button>("STARTOVER");
-        resume = menu.Q<Button>("RESUME");
-        exit = menu.Q<Button>("EXIT");
+        startoverBtn = menu.Q<Button>("STARTOVER");
+        resumeBtn = menu.Q<Button>("RESUME");
+        exitBtn = menu.Q<Button>("EXIT");
 
         // default hide the menu
         menu.style.display = DisplayStyle.None;
+
+        // adding the button event listeners
+        startoverBtn.clicked += () => StartOver();
+        resumeBtn.clicked += () => Resume();
+        exitBtn.clicked += () => Exit();
     }
 
     // Update is called once per frame
@@ -37,14 +50,30 @@ public class UI : MonoBehaviour
         if (keyboard != null)
         {
             if (keyboard.escapeKey.isPressed) menu.style.display = DisplayStyle.Flex;
+            // show the mouse
+            UnityEngine.Cursor.lockState = CursorLockMode.None;
+            UnityEngine.Cursor.visible = true;
         }
-        // if exit button clicked, quit game
-        exit.clicked += () =>
-        {
-            // force stop unity editor
-            UnityEditor.EditorApplication.isPlaying = false;
-            // force stop the game
-            Application.Quit();
-        };
+    }
+
+    void StartOver()
+    {
+        // reload the main scene
+        SceneManager.LoadScene("Main");
+    }
+    void Resume()
+    {
+        // hide the mouse
+        UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+        UnityEngine.Cursor.visible = false;
+        // hide the menu
+        menu.style.display = DisplayStyle.None;
+    }
+    void Exit()
+    {
+        // force stop unity editor
+        UnityEditor.EditorApplication.isPlaying = false;
+        // force stop the game
+        Application.Quit();
     }
 }
