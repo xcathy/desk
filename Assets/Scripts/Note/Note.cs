fa62134d14a62984009c0dde1e8d6eca929a78cc
xcathy <cathyxie1997@hotmail.com>
2026-03-15 15:46:02 +0800

    added dialogues

---
+++ Assets/Scripts/Note/Note.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Note : MonoBehaviour, IObject
+{
+    // UI refs
+    public UIDocument UIDoc;
+    // private vars
+    private VisualElement root;
+    private VisualElement dialogue;
+    private Label text;
+    
+    public void Hover()
+    {}
+    public void Unhover()
+    {}
+    public void LeftClick()
+    {
+        // shows dialogue when clicked
+        dialogue.style.display = DisplayStyle.Flex;
+        text.text = "What is this...?";
+    }
+
+    void Start()
+    {
+        root = UIDoc.rootVisualElement;
+        dialogue = root.Q<VisualElement>("dialogue");
+        text = root.Q<Label>("text");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
